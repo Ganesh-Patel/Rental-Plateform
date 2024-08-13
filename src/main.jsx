39b6ce2619825be5d3ee05b80eed732beef57161
Ganesh <ganesh.oficial158@gmail.com>
2024-08-13 16:20:07 +0530

    Routing fixed

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { PropertiesContext } from './Components/MyContext/Propertiescontext.js'
+import { PropertiesContext } from './Components/MyContext/PropertiesContext.js'
 import properties from './Components/Data/Properties.js'
 import { BrowserRouter as Router } from 'react-router-dom';
-import { CartProvider } from './Components/MyContext/Cartcontext'
+import { CartProvider } from './Components/MyContext/CartContext'
 
 console.log(properties)
 
